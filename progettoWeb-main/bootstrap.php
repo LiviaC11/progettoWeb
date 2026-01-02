29c0726341d59fea2058109df5ad70d538db1314
Livia <livia.cicognani@studio.unibo.it>
2026-01-02 12:02:14 +0100

    Aggiornato bootstrap.php con sessioni e funzioni utils

--- progettoWeb-main/bootstrap.php
+++ progettoWeb-main/bootstrap.php
@@ -2,6 +2,9 @@
 session_start();
 define("UPLOAD_DIR", "./upload/");
 
+// Carica le funzioni di utilità (corretto con la 's' come richiesto)
+require_once("utils/functions.php");
+
 // Qui in futuro includerai il database:
 // require_once("db/database.php");
 // $dbh = new DatabaseHelper(...);
