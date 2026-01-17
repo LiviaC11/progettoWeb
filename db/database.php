1dbdb95d0dbb0bcaa64c270cae6afab619687348
Livia <livia.cicognani@studio.unibo.it>
2026-01-17 10:38:16 +0100

    Aggiunto db, piccola modifica in home per problema di contrasto colore, aggiunta una funzione in dbHelper e modificata una aggiungendo info da database

--- db/database.php
+++ db/database.php
@@ -9,7 +9,20 @@ class DatabaseHelper {
             die("Connessione fallita: " . $this->db->connect_error);
         }
     }
+    public function checkLogin($email, $password) {
+        $query = "SELECT id_utente, nome, email, password, ruolo, id_casa FROM utenti WHERE email = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('s', $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $user = $result->fetch_assoc();
 
+        // Verifichiamo l'hash della password
+        if ($user && password_verify($password, $user['password'])) {
+            return $user; // Ritorna i dati dell'utente (senza la password)
+        }
+        return false;
+    }
     // inseririmento nuova candidatura
     public function insertCandidatura($id_annuncio, $nome, $email, $messaggio, $foto) {
         $query = "INSERT INTO candidature (id_annuncio, nome, email, messaggio, foto) VALUES (?, ?, ?, ?, ?)";
@@ -21,13 +34,28 @@ class DatabaseHelper {
     
     public function getRandomAnnunci($n) {
     // Seleziona n annunci in ordine casuale
-    $query = "SELECT * FROM annunci ORDER BY RAND() LIMIT ?";
+    $query = "SELECT a.*, u.nome as nome_proprietario 
+                  FROM annunci a 
+                  LEFT JOIN utenti u ON a.id_utente = u.id_utente 
+                  ORDER BY RAND() LIMIT ?";
     $stmt = $this->db->prepare($query);
     $stmt->bind_param('i', $n);
     $stmt->execute();
     $result = $stmt->get_result();
 
     return $result->fetch_all(MYSQLI_ASSOC);
+}
+public function getPulizieByCasa($id_casa){
+    $query= "SELECT t.*, u.nome as assegnato_a_nome
+             FROM turni_pulizie t
+             JOIN utenti u ON t.assegnato_a = u.id_utente
+             WHERE t.id_casa = ?
+             ORDER BY t.data_scadenza ASC";
+    $stmt= $this->db->prepare($query);
+    $stmt->bind_param('i', $id_casa);
+    $stmt->execute();
+    return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+
 }
 }
 ?>
